980070858f30cd97d86ee04e3e0fcee19e8e5d12
Stefan <stefststef72@yahoo.com>
2024-12-07 00:43:07 +0200

    added flyer table

---
+++ src/content.js
@@ -0,0 +1,21 @@
+flyer = `<h1 class = "contentTitle">Dummy Text</h1>
+        <div class = "tableHolder">
+            <table>
+                <tr class = "tableHead">
+                    <td>LUNA</td>
+                    <td>AN</td>
+                    <td>FLUTURAS</td>
+                </tr>
+                <tr class = "dataLine">
+                    <td class = "month">Dummy</td>
+                    <td class = "year">9999</td>
+                    <td class = "download">
+                        <div>
+                            Descarca
+                            <img src="resources/file_download.png"
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+`
--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
-console.log
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelector('.content').innerHTML = `
+        <h2>Meniu Principal</h2>
+        <p>Aici sunt elementele meniului principal...</p>
+    `;
+});
+
+document.querySelector('.fluturas').addEventListener('click', () => {
+    document.querySelector('.content').innerHTML = flyer;
+});
+
+document.querySelector('.pontaj').addEventListener('click', () => {
+    document.querySelector('.content').innerHTML = `
+        <h2>Pontaj</h2>
+        <p>Aici se află pontajul tău...</p>
+    `;
+});
+
+document.querySelector('.concedii').addEventListener('click', () => {
+    document.querySelector('.content').innerHTML = `
+        <h2>Concedii</h2>
+        <p>Concedii ce necesită aprobare...</p>
+    `;
+});
+
+document.querySelector('.rapoarte').addEventListener('click', () => {
+    document.querySelector('.content').innerHTML = `
+        <h2>Rapoarte</h2>
+        <p>Aici se află rapoartele tale...</p>
+    `;
+});
+
+document.querySelector('.cereri').addEventListener('click', () => {
+    document.querySelector('.content').innerHTML = `
+        <h2>Cereri de concediu</h2>
+        <p>Poți crea o cerere nouă de concediu aici...</p>
+    `;
+});
