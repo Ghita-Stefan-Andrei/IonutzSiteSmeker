5e04ac2434a4412d9dd237e3f3e6f2a0e2ae17ab
Stefan <stefststef72@yahoo.com>
2024-12-07 20:16:39 +0200

    database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!")
+const express = require('express');
+const cors = require('cors'); 
+const fs = require('fs');
+const app = express();
+
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+    fs.readFile('./db/fluturas.json', 'utf8', (err, jsonData) => {
+        if (err) {
+            console.error('Eroare la citirea fișierului JSON:', err);
+            res.status(500).send('Eroare server!');
+        } else {
+            const data = JSON.parse(jsonData); 
+            res.json(data); 
+        }
+    });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ document.querySelector('.menu').addEventListener('click', () => {
     document.querySelector('.content').innerHTML = getMenu();
 });
 
-document.querySelector('.fluturas').addEventListener('click', () => {
-    const data = 
-    {
-        month: ["January", "March"],
-        year: [2023, 2024]
-    };
-    document.querySelector('.content').innerHTML = getFlyer(data);
+document.querySelector('.fluturas').addEventListener('click', async () => {
+    try {
+        const response = await fetch('http://localhost:3000/api/data');
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+        
+        document.querySelector('.content').innerHTML = getFlyer(data);
+    } catch (error) {
+        console.error('Eroare la obținerea datelor:', error);
+    }
 });
 
 document.querySelector('.pontaj').addEventListener('click', () => {
